04357785911ac2aad8e6eba23432b1946bfc49fd
RennoGarcia <renno_oq@hotmail.com>
2018-03-09 20:04:11 -0300

    q1
    atividade

---
+++ q01.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+   
+    int main(void)
+    {
+        int cont = 0, Num;
+        printf("Digite um numero inteiro:");
+        scanf("%d", &Num);
+        if (Num == 0) cont = 1; //verifica se o numero é igual a zero. se não for, o número será testado. 
+        else //não sendo zero, contador adiciona 1.
+            while (Num != 0)//se o numero for diferente de zero e maior que 10, então ele é no mínimo divizivel 1 vez por 10.
+           {
+               cont= cont+ 1;
+               Num = Num/10;
+           }//caso o numero for maior que 2 digitos, o numero será dividiro por 10 até que não possa mais.
+           
+       printf("Este número possúi %d dígitos", cont);
+       return 0;
+  }
